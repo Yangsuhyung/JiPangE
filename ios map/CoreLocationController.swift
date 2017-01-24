2842087c86d7c9dfb5171e3ed9b62113a1aefbd3
cscoi054 <eatyourmeat1@hanmail.net>
2017-01-24 17:55:14 +0900

    submit

---
+++ ios map/CoreLocationController.swift
@@ -0,0 +1,11 @@
+//
+//  CoreLocationController.swift
+//  ios map
+//
+//  Created by cscoi054 on 2017. 1. 24..
+//  Copyright © 2017년 a. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
